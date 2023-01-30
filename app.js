35422a7bdce5219d16c61dbdd143dcc2e227536f
satyamsb1 <satyamborade45@gmail.com>
2023-01-30 20:56:54 +0530

    Code of the day 1 of react course

---
+++ app.js
@@ -0,0 +1,22 @@
+const heading = React.createElement("h1", {
+    id: "title"
+}, 
+"heading 1");
+
+
+const heading2 = React.createElement("h2", {
+    id: "title"
+}, "heading 2");
+
+
+const container = React.createElement("div", {
+        id: "container"
+    }, 
+    [heading, heading2]
+);//these are react element
+
+console.log(container);
+
+root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);//render will modify / override
